73ccae09f7e433a6e23c72681c64adff307e4b6a
hayung <yoohayung10309@gmail.com>
2023-12-27 17:16:33 +0900

    딱히 switch 문 필요없는 형식

--- HelloWorld/Building_Floor.java
+++ HelloWorld/Building_Floor.java
@@ -11,26 +11,27 @@ public class Building_Floor {
         System.out.println("1층부터 5층까지 선택해주세요: ");
         int floor = Integer.parseInt(br.readLine());
         String[] floor_name = { "약국", "정형외과", "피부과", "치과", "헬스장" };
-        switch (floor) {
-            case 1:
-                sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
-                break;
-            case 2:
-                sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
-                break;
-            case 3:
-                sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
-                break;
-            case 4:
-                sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
-                break;
-            case 5:
-                sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
-                break;
+        // switch (floor) {
+        //     case 1:
+        //         sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
+        //         break;
+        //     case 2:
+        //         sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
+        //         break;
+        //     case 3:
+        //         sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
+        //         break;
+        //     case 4:
+        //         sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
+        //         break;
+        //     case 5:
+        //         sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
+        //         break;
 
-        }
+        // }
+        sb.append(floor).append("층 ").append(floor_name[floor - 1]).append("입니다.");
         System.out.println(sb);
         br.close();
-        
+
     }
 }
