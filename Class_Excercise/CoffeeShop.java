f9d9b838d75fc5097d2f440f895bdec49aef9fc1
hayung <yoohayung10309@gmail.com>
2024-01-04 09:22:19 +0900

    변수 private static으로 선언 - > set,get 을 static 으로 접근

--- Class_Excercise/CoffeeShop.java
+++ Class_Excercise/CoffeeShop.java
@@ -9,23 +9,23 @@ public class CoffeeShop {
 
     StringBuilder sb = new StringBuilder();
 
-    public CoffeeShop(String shopName) {
+    public CoffeeShop(String shopName) {  //커피숍 생성자
         this.shopName = shopName;
 
     }
 
-    public void coffeeOrder(String drink, int price) {
-        this.drink.add(drink);
+    public void coffeeOrder(String drink, int price) {// 어떤 음료를 얼마에 샀는지 음료 주문 
+        this.drink.add(drink);  
         this.money += price;
     }
 
-    public void showinfo() {
+    public void showinfo() {  //커피숍에서 어떤 음료를 구매하였고, 매출은 총합 얼마가 나왔는지 
 
         sb.append(shopName + "의 판매 물품은");
         for (int i = 0; i < drink.size(); i++) {
             sb.append(drink.get(i)).append(" ");
         }
-        sb.append("이고, 판매 액은 " + this.money + "원 입니다.").append("\n");
+        sb.append("이고, 판매 액은 " + this.money + "원 입니다.");
         System.out.println(sb);
     }
 }
--- Class_Excercise/Worker.java
+++ Class_Excercise/Worker.java
@@ -4,19 +4,19 @@ public class Worker {
     String workerName;
     int money;
     
-    public Worker(String workerName , int money){
+    public Worker(String workerName , int money){ //직장인 생성자
         this.workerName = workerName;
         this.money = money;
     }
-    public void GetAmericano(CoffeeShop coffeeshop){
+    public void GetAmericano(CoffeeShop coffeeshop){ //아메리카노 4000원 구입
         this.money -= 4000;
         coffeeshop.coffeeOrder("americano",4000);
     }
-    public void GetCafeLate(CoffeeShop coffeeshop){
-        this.money -= 4500;
+    public void GetCafeLate(CoffeeShop coffeeshop){   //카페라떼 4500원 구입
+        this.money -= 4500;  
         coffeeshop.coffeeOrder("CafeLate",4500);
     }
-    public void showInfo(){
+    public void showInfo(){  //직장인의 남은 돈 계산
         System.out.println(workerName + "님의 남은 돈은 "+money+"입니다.");
     }
 }
