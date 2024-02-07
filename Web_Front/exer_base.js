1aa29901c70cd4acccaf8e95ae0bfbf03391b53f
hayung <yoohayung10309@gmail.com>
2024-02-07 09:05:40 +0900

    제이쿼리 2.6(화) 설명 $("h1")

--- Web_Front/exer_base.js
+++ Web_Front/exer_base.js
@@ -1,1 +1,9 @@
-console.log("wow");
+var scores = [1, 2, 3, 4, 5, 6];
+var totalScores = 0;
+
+for (var i = 0; i < scores.length; i++) {
+  totalScores = totalScores + scores[i];
+}
+var averageScores = Math.round(totalScores / scores.length);
+console.log(averageScores);
+document.write(averageScores + "<br>");
